e92812aae94beaba0815df9040b32baa4719142e
yangtaodev <yangtao.work@qq.com>
2017-08-02 13:20:09 +0800

    [Feature]init database use litepal,create three table province city county
    [id]:
    
    [Type]
      <Feature>
    
    [Projects]
      <X_HIKe/Z_Baseline>
    
    [Solution Description]
    
    [Other info]

--- app/src/main/java/com/android/tony/weather/MainActivity.java
+++ app/src/main/java/com/android/tony/weather/MainActivity.java
@@ -3,11 +3,14 @@ package com.android.tony.weather;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.litepal.LitePal;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LitePal.getDatabase();
     }
 }
